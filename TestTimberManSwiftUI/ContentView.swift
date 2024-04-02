7c3bdc2b3900e79eb80c93a796a9f646493b88cb
brunothuma <bruno.thuma@hotmail.com>
2024-04-02 14:06:50 -0300

    Initial Commit

---
+++ TestTimberManSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTimberManSwiftUI
+//
+//  Created by Bruno Thuma on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTimberManSwiftUI/TestTimberManSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTimberManSwiftUIApp.swift
+//  TestTimberManSwiftUI
+//
+//  Created by Bruno Thuma on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTimberManSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
