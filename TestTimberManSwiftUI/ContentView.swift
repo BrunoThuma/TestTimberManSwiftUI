f58675385eb2e4a3e529ce032c2e3019082810e8
brunothuma <bruno.thuma@hotmail.com>
2024-04-03 04:10:58 -0300

    shows banner and reward

--- TestTimberManSwiftUI/ContentView.swift
+++ TestTimberManSwiftUI/ContentView.swift
@@ -7,8 +7,23 @@
 
 import SwiftUI
 import SpriteKit
+import GoogleMobileAds
 
 struct ContentView: View {
+    #if DEBUG
+    let bannerAdId =  "ca-app-pub-3940256099942544/2435281174"
+    let rewardAdId = "ca-app-pub-3940256099942544/5224354917"
+    #else
+    let bannerAdId = "Seu id de banner"
+    let rewardAdId = "Seu id de reward"
+    #endif
+    
+    @State var shouldPresentRewardAd = false {
+        didSet {
+            print(shouldPresentRewardAd)
+        }
+    }
+    
     var scene: SKScene {
         let scene = GameScene(fileNamed: "GameScene")
         scene!.scaleMode = .fill
@@ -20,6 +35,33 @@ struct ContentView: View {
             SpriteView(scene: scene)
                 .ignoresSafeArea()
                 .navigationBarBackButtonHidden(true)
+            
+            VStack() {
+                Button(action: {
+                    shouldPresentRewardAd = true
+                }, label: {
+                    Text("Present Reward")
+                })
+                .buttonStyle(BorderedProminentButtonStyle())
+                Spacer()
+                BannerAd(adUnitId: bannerAdId)
+                    .frame(width: GADAdSizeBanner.size.width,
+                           height: GADAdSizeBanner.size.height)
+            }
+            if shouldPresentRewardAd {
+                RewardedAdView(
+                    isPresented: $shouldPresentRewardAd,
+                    adUnitId: rewardAdId,
+                    rewardFunc: {
+                    print("did present reward video")
+                })
+            }
+            
+            
+        }
+        .onAppear {
+            RewardedAd.shared.loadAd(withAdUnitId: "ca-app-pub-3940256099942544/5224354917")
+            print("Loading intersticial ad")
             
         }
     }
